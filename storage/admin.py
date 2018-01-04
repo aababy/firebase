f9603e5453acec1ba2d17389c2bfdd885d3c3ca6
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-01-04 15:43:29 +0800

    no message

--- storage/admin.py
+++ storage/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from django.contrib import messages
 from storage.models import App, Tag, Graph
 
+class GraphAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display', 'url')
+    readonly_fields = ('display',)
+
+
 admin.site.register(App)
 admin.site.register(Tag)
-admin.site.register(Graph)
+admin.site.register(Graph, GraphAdmin)
