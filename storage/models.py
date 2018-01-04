f8fc50e96a967f4df92649de11dce22c23535106
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-01-04 14:29:51 +0800

    创建数据库

--- storage/models.py
+++ storage/models.py
@@ -1,5 +1,36 @@
+#coding:utf-8
+
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Create your models here.
+class App(models.Model):
+    name = models.CharField(max_length=200)
+    tag_order = models.CharField(max_length=400)
+
+    def __unicode__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    image_order = models.CharField(max_length=400)
+
+    app = models.ManyToManyField(App)   # app
+
+    def __unicode__(self):
+        return self.name
+
+class Graph(models.Model):
+    name = models.CharField(max_length=200)             # 姓名
+    url = models.CharField(max_length=400, default="")  # URL
+
+    def display(self):
+        return mark_safe(u'<img src="%s" width="120px" />' % self.url)
+    display.short_description = u'图片'
+
+    tag = models.ManyToManyField(Tag)   # tag
+
+    def __unicode__(self):
+        return self.name
