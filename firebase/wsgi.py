9a509f3b3573a155a7d3d4c2cd88d9eb6151a3ae
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-03-28 15:35:52 +0800

    no message

--- firebase/wsgi.py
+++ firebase/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-import uwsgi
+# import uwsgi
 from cron_job import *
 
 from django.core.wsgi import get_wsgi_application
@@ -17,9 +17,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firebase.settings")
 
 application = get_wsgi_application()
 
-for job_id, job in enumerate(jobs):
-    uwsgi.register_signal(job_id, "", job['name'])
-    if len(job['time']) == 1:
-        uwsgi.add_timer(job_id, job['time'][0])
-    else:
-        uwsgi.add_cron(job_id, job['time'][0], job['time'][1], job['time'][2], job['time'][3], job['time'][4])
+# for job_id, job in enumerate(jobs):
+#     uwsgi.register_signal(job_id, "", job['name'])
+#     if len(job['time']) == 1:
+#         uwsgi.add_timer(job_id, job['time'][0])
+#     else:
+#         uwsgi.add_cron(job_id, job['time'][0], job['time'][1], job['time'][2], job['time'][3], job['time'][4])
