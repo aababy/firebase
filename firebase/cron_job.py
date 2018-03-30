c4fc37f85c30104dc646e4e2c7972df11205ff6f
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-03-30 15:39:44 +0800

    no message

--- firebase/cron_job.py
+++ firebase/cron_job.py
@@ -1,20 +1,24 @@
-#coding:utf-8
+# coding:utf-8
 import time
 import logging
 
+
 def cron_print_time(signum):
-    ISOTIMEFORMAT='%Y-%m-%d %X'
-    print time.strftime(ISOTIMEFORMAT, time.localtime())
+    ISOTIMEFORMAT = '%Y-%m-%d %X'
+    log = time.strftime(ISOTIMEFORMAT, time.localtime())
+    mylog = logging.getLogger('django')
+    mylog.debug(log)
+
 
 def cron_print_hello(signum):
-    mylog = logging.getLogger('django')
-    mylog.debug('@@hello')
     print "hello"
 
-jobs = [ { "name" : cron_print_time,
-           "time": [0, 17, -1, -1, 1], #minute, hour, day, month, weekday, "-1" means "all"，此例为每个周一的17：00
-          },     
-         { "name" : cron_print_hello,
-           "time": [5],  #每隔2秒
-          },    
-]
+
+jobs = [{"name": cron_print_time,
+         # minute, hour, day, month, weekday, "-1" means "all"，此例为每个周一的17：00
+         "time": [0, 17, -1, -1, -1],
+         },
+        # {"name": cron_print_hello,
+        #  "time": [5],  # 每隔2秒
+        #  },
+        ]
--- firebase/wsgi.py
+++ firebase/wsgi.py
@@ -7,19 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
 import uwsgi
+import os
+from django.core.wsgi import get_wsgi_application
 from firebase.cron_job import *
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firebase.settings")
 
 application = get_wsgi_application()
 
-# for job_id, job in enumerate(jobs):
-#     uwsgi.register_signal(job_id, "", job['name'])
-#     if len(job['time']) == 1:
-#         uwsgi.add_timer(job_id, job['time'][0])
-#     else:
-#         uwsgi.add_cron(job_id, job['time'][0], job['time'][1], job['time'][2], job['time'][3], job['time'][4])
+for job_id, job in enumerate(jobs):
+    uwsgi.register_signal(job_id, "", job['name'])
+    if len(job['time']) == 1:
+        uwsgi.add_timer(job_id, job['time'][0])
+    else:
+        uwsgi.add_cron(job_id, job['time'][0], job['time'][1], job['time'][2], job['time'][3], job['time'][4])
