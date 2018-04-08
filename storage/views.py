87695e13f60dba9ce60778e957c7f014d48c00c2
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-04-08 10:27:12 +0800

    no message

--- storage/views.py
+++ storage/views.py
@@ -53,7 +53,7 @@ def ajax_publish(request):
             jsonData.append(data)
 
     elif name == 'package':
-        for query in Package.objects.order_by("-name"):
+        for query in Package.objects.order_by("name"):
             data = {}
             data['name'] = query.name
             data['thumb_name'] = query.thumb_name
