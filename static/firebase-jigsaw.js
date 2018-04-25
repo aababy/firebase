57c71a533aea1029539df4864c1e5828b88a2c21
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-04-25 15:28:07 +0800

    no message

---
+++ static/firebase-jigsaw.js
@@ -0,0 +1,15 @@
+var config = {
+    // apiKey: "AIzaSyBeREVjj79wuwgHlvTVnslVim0NEBwviy8",
+    // authDomain: "tick-tock-apps.firebaseapp.com",
+    // databaseURL: "https://tick-tock-apps.firebaseio.com",
+    // projectId: "tick-tock-apps",
+    // storageBucket: "tick-tock-apps.appspot.com",
+    // messagingSenderId: "743557369082"
+
+    apiKey: "AIzaSyASpCyzRmmwVlQUqG7G-ArJ31iogpM5auI",
+    authDomain: "test-project-33489.firebaseapp.com",
+    databaseURL: "https://test-project-33489.firebaseio.com",
+    projectId: "test-project-33489",
+    storageBucket: "test-project-33489.appspot.com",
+    messagingSenderId: "493620055150"
+};
