6153c104db7be9a21aab2a56965ba4c00adda34f
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-08-08 08:27:08 +0800

    no message

--- firebase/wsgi.py
+++ firebase/wsgi.py
@@ -17,11 +17,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firebase.settings")
 application = get_wsgi_application()
 
 for job_id, job in enumerate(jobs):
-
-    print 'xxxx'
-
     uwsgi.register_signal(job_id, "", job['name'])
-    if len(job['time']) == 1:
-        uwsgi.add_timer(job_id, job['time'][0])
-    else:
-        uwsgi.add_cron(job_id, job['time'][0], job['time'][1], job['time'][2], job['time'][3], job['time'][4])
+#     if len(job['time']) == 1:
+#         uwsgi.add_timer(job_id, job['time'][0])
+#     else:
+#         uwsgi.add_cron(job_id, job['time'][0], job['time'][1], job['time'][2], job['time'][3], job['time'][4])
