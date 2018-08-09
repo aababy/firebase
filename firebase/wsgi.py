dd8da3d494e433cab151263db4c935bfb9ad9c91
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-08-09 16:14:12 +0800

    no message

--- firebase/wsgi.py
+++ firebase/wsgi.py
@@ -18,7 +18,7 @@ application = get_wsgi_application()
 
 for job_id, job in enumerate(jobs):
     uwsgi.register_signal(job_id, "", job['name'])
-#     if len(job['time']) == 1:
-#         uwsgi.add_timer(job_id, job['time'][0])
-#     else:
-#         uwsgi.add_cron(job_id, job['time'][0], job['time'][1], job['time'][2], job['time'][3], job['time'][4])
+    if len(job['time']) == 1:
+        uwsgi.add_timer(job_id, job['time'][0])
+    else:
+        uwsgi.add_cron(job_id, job['time'][0], job['time'][1], job['time'][2], job['time'][3], job['time'][4])
