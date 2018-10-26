289b0b9c9cabf1e6a1a5329b4f2f963658e98ed1
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-10-26 15:17:48 +0800

    no message

--- static/firebase-jigsaw.js
+++ static/firebase-jigsaw.js
@@ -1,15 +1,15 @@
 var config = {
-    // apiKey: "AIzaSyCD4rDC-GWI0-jkmfRmgvdH0PVL_aaa7nE",
-    // authDomain: "big-bear-entertainment.firebaseapp.com",
-    // databaseURL: "https://big-bear-entertainment.firebaseio.com",
-    // projectId: "big-bear-entertainment",
-    // storageBucket: "big-bear-entertainment.appspot.com",
-    // messagingSenderId: "391712938904"
+    apiKey: "AIzaSyCD4rDC-GWI0-jkmfRmgvdH0PVL_aaa7nE",
+    authDomain: "big-bear-entertainment.firebaseapp.com",
+    databaseURL: "https://big-bear-entertainment.firebaseio.com",
+    projectId: "big-bear-entertainment",
+    storageBucket: "big-bear-entertainment.appspot.com",
+    messagingSenderId: "391712938904"
 
-    apiKey: "AIzaSyBvcx_zTTFWyOlkE8x1y1_I1bSl3zaFkrA",
-    authDomain: "hidd-bigb001.firebaseapp.com",
-    databaseURL: "https://hidd-bigb001.firebaseio.com",
-    projectId: "hidd-bigb001",
-    storageBucket: "hidd-bigb001.appspot.com",
-    messagingSenderId: "278231656471"
+    // apiKey: "AIzaSyBvcx_zTTFWyOlkE8x1y1_I1bSl3zaFkrA",
+    // authDomain: "hidd-bigb001.firebaseapp.com",
+    // databaseURL: "https://hidd-bigb001.firebaseio.com",
+    // projectId: "hidd-bigb001",
+    // storageBucket: "hidd-bigb001.appspot.com",
+    // messagingSenderId: "278231656471"
 };
