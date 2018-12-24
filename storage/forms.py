0f867840e1cb5b47b58e0225ec3ec7b8267366de
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2018-12-24 12:31:07 +0800

    no message

--- storage/forms.py
+++ storage/forms.py
@@ -5,5 +5,5 @@ from .models import Graph, Category
 class CategoryForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CategoryForm, self).__init__(*args, **kwargs)
-        self.fields['cover'].queryset = Graph.objects.filter(tag=self.instance)
+        self.fields['cover'].queryset = Graph.objects.filter(category=self.instance)
         
